ea245b30cda587cb31d49f62e924a6c6d8afa600
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2025-08-29 18:44:01 +0200

    fix: minimap has the direction line

--- src_bonus/raycasting/rays_on_minimap.c
+++ src_bonus/raycasting/rays_on_minimap.c
@@ -71,20 +71,3 @@ void	draw_ray_on_minimap(t_game *game, double ray_dir_x, double ray_dir_y)
 	}
 }
 
-void	draw_all_rays_on_minimap(t_game *game)
-{
-	int		screen_x;
-	double	camera_x;
-	double	ray_dir_x;
-	double	ray_dir_y;
-
-	screen_x = 0;
-	while (screen_x < game->mlx.width)
-	{
-		camera_x = 2 * screen_x / (double)game->mlx.width - 1;
-		ray_dir_x = game->player.dir_x + game->player.plane_x * camera_x;
-		ray_dir_y = game->player.dir_y + game->player.plane_y * camera_x;
-		draw_ray_on_minimap(game, ray_dir_x, ray_dir_y);
-		screen_x += 10;
-	}
-}
